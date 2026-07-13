1817b787644760118cf02b1091e3c33a193ad5d8
stevancosovic <stevan.cosovic777@gmail.com>
2026-07-13 19:59:35 +0200

    testing controllers - done

---
+++ LeaveManagementSystemDemo/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagementSystemDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystemDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            TestViewModel model = new();
+
+            return View(model);
+        }
+    }
+}
---
+++ LeaveManagementSystemDemo/Models/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeaveManagementSystemDemo.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Id { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
