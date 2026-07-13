e72c653c127c2b4da83502e7405a0d0e0c7d4723
stevancosovic <stevan.cosovic777@gmail.com>
2026-07-13 19:51:17 +0200

    Add project files.

---
+++ LeaveManagementSystemDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementSystemDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
